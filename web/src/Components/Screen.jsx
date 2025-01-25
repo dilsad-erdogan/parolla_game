a66508959a71404a1161939d818f2088f1b7ce14
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-01-25 23:09:40 +0300

    game design

--- web/src/Components/Screen.jsx
+++ web/src/Components/Screen.jsx
@@ -24,56 +24,40 @@ const Screen = () => {
     };
 
     return (
-        <div>
+        <div className="container mx-auto mt-10">
             {currentQuestion ? (
-                <div>
-                    <h2 className="text-lg font-semibold">
-                        Category: {currentQuestion.categoryName}
-                    </h2>
-                    <p>Question: {currentQuestion.question}</p>
-                    <input
-                        type="text"
-                        className="bg-gray-700 border p-2 rounded-lg mt-4"
-                        placeholder="Your answer"
-                        value={userAnswer}
-                        onChange={(e) => setUserAnswer(e.target.value)}
-                    />
-                    <button
-                        className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg mt-4"
-                        onClick={handleAnswer}
-                    >
-                        Submit Answer
-                    </button>
+                <div className="h-[900px] flex flex-col items-center justify-between">
+                    <div className="bg-gray-700 rounded-full max-w-32 max-h-32 w-24 h-24 p-5">
+                        <h2 className="text-center text-5xl font-bold">{currentQuestion.categoryName}</h2>
+                    </div>
+
+                    <div className="text-2xl font-bold">
+                        <p>{currentQuestion.question}</p>
+                    </div>
+
+                    <div className="flex gap-5 w-full justify-center">
+                        <input type="text" className="bg-gray-700 border p-2 rounded-lg mt-4" placeholder="Your answer" value={userAnswer} onChange={(e) => setUserAnswer(e.target.value)} />
+                        <button className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg mt-4" onClick={handleAnswer}>Pas</button>
+                    </div>
                 </div>
             ) : (
-                <p>No more questions available.</p>
+                <p>Your test is done.</p>
             )}
 
             {/* Modal */}
-            <Modal
-                isOpen={showModal}
-                onRequestClose={() => setShowModal(false)}
-                className="bg-gray-800 text-white p-8 rounded-lg max-w-md mx-auto mt-20"
-                overlayClassName="fixed inset-0 bg-black bg-opacity-50"
-            >
+            <Modal isOpen={showModal} onRequestClose={() => setShowModal(false)} className="bg-gray-800 text-white flex-1 overflow-y-auto p-8 rounded-lg max-w-3xl mx-auto mt-20" overlayClassName="fixed inset-0 bg-black bg-opacity-50">
                 <h2 className="text-xl font-bold mb-4">Game Over</h2>
-                <p className="mb-4">Your Results:</p>
                 <ul className="mb-4">
                     {questions.map((question, index) => (
                         <li key={index} className="mb-2">
-                            <span>
+                            <span className={`${isCorrect[index] ? 'text-green-500' : 'text-red-500'}`}>
                                 {index + 1}. {question.question} -{" "}
                                 {isCorrect[index] ? "Correct" : "Wrong"}
                             </span>
                         </li>
                     ))}
                 </ul>
-                <button
-                    className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg"
-                    onClick={() => setShowModal(false)}
-                >
-                    Close
-                </button>
+                <button className="bg-yellow-500 hover:bg-yellow-600 text-white py-2 px-4 rounded-lg" onClick={() => setShowModal(false)}>Close</button>
             </Modal>
         </div>
     );
